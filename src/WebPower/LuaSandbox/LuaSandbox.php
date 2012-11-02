701c896fbe6f1ef01efbbd1008156756adcc5f82
Christiaan <anotherhero@gmail.com>
2012-11-02 11:09:08 +0100

    More tests

--- src/WebPower/LuaSandbox/LuaSandbox.php
+++ src/WebPower/LuaSandbox/LuaSandbox.php
@@ -5,60 +5,92 @@ use \Lua;
 
 class LuaSandbox
 {
-	/** @var Lua */
-	private $sandbox;
+    /** @var Lua */
+    private $sandbox;
 
-	function __construct()
-	{
-		if (!class_exists('\\Lua')) {
-			throw new Exception('Lua PHP module not installed. See http://pecl.php.net/package/lua');
-		}
-		$this->sandbox = new Lua();
+    function __construct()
+    {
+        if (!class_exists('\\Lua')) {
+            throw new Exception('Lua PHP module not installed. See http://pecl.php.net/package/lua');
+        }
+        $this->sandbox = new Lua();
+    }
 
-		$this->unsetGlobals(array(
-			'collectgarbage',
-			'coroutine.create',
-			'coroutine.running',
-			'coroutine',
-			'debug',
-			'dofile',
-			'gcinfo',
-		));
+    /**
+     * @param string $lua
+     * @return int|float|string|array|callable|void
+     * @throws LuaErrorException
+     */
+    public function run($lua)
+    {
+        $level = error_reporting(0);
 
-	}
+        $retval = $this->sandbox->eval($lua);
 
-	private function unsetGlobals($globals)
-	{
-		foreach ($globals as $global) {
-			$this->sandbox->eval('_G.'.$global.' = nil');
-		}
-	}
+        error_reporting($level);
 
-	/**
-	 * @param string $lua
-	 * @return int|float|string|array|callable|void
-	 * @throws LuaErrorException
-	 */
-	public function execute($lua)
-	{
-		$level = error_reporting(0);
+        if ($retval === false) {
+            $error = error_get_last();
+            $this->throwLuaError($error);
+        }
+        return $retval;
+    }
 
-		$retval = $this->sandbox->eval($lua);
+    public function assignCallable($name, $callback)
+    {
+        $this->verifyVariableName($name);
+        if (!is_callable($callback)) {
+            throw new \InvalidArgumentException('Callback should be a callable');
+        }
+        $this->sandbox->registerCallback($name, $callback);
+    }
 
-		error_reporting($level);
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @throws Exception when lua didn't register the var
+     */
+    public function assignVar($name, $value)
+    {
+        $this->verifyVariableName($name);
+        $this->sandbox->assign($name, $value);
+        $ret = $this->run('return _G["'.$name.'"]');
+        if ($ret != $value) {
+            $this->unsetVar($name);
+            throw new Exception(sprintf('Assigning Var with name: %s failed', $name));
+        }
+    }
 
-		if ($retval === false) {
-			$error = error_get_last();
-			$this->throwLuaError($error);
-		}
-		return $retval;
-	}
+    public function unsetVar($name)
+    {
+        foreach ((array) $name as $global) {
+            $this->sandbox->assign($global, null);
+        }
+    }
 
-	private function throwLuaError($error)
-	{
-		$error = new \ErrorException(
-			$error['message'], 0, $error['type'], $error['file'], $error['line']
-		);
-		throw new LuaErrorException('Error in executed Lua', 0, $error);
-	}
+    private function verifyVariableName($name)
+    {
+        $reserved = array(
+            'and', 'break', 'do', 'else', 'elseif', 'end', 'false', 'for',
+            'function', 'if', 'in', 'local', 'nil', 'not', 'or', 'repeat',
+            'return', 'then', 'true', 'until', 'while'
+        );
+
+        $isReserved = in_array($name, $reserved);
+        $isInvalid = !preg_match(
+            '/^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*/',
+            $name
+        );
+        if ($isReserved || $isInvalid) {
+            throw new InvalidVariableNameException($name);
+        }
+    }
+
+    private function throwLuaError($error)
+    {
+        $error = new \ErrorException(
+            $error['message'], 0, $error['type'], $error['file'], $error['line']
+        );
+        throw new LuaErrorException('Error in executed Lua', 0, $error);
+    }
 }
--- tests/WebPower/LuaSandbox/Tests/LuaSandboxTest.php
+++ tests/WebPower/LuaSandbox/Tests/LuaSandboxTest.php
@@ -5,93 +5,181 @@ use WebPower\LuaSandbox\LuaSandbox;
 
 class LuaSandboxTest extends \PHPUnit_Framework_TestCase
 {
-	function testCreation()
-	{
-		new LuaSandbox();
-	}
+    /**
+     * @var LuaSandbox
+     */
+    public $obj;
 
-	function testInvalidLuaThrowsException()
-	{
-		$this->setExpectedException('\WebPower\LuaSandbox\LuaErrorException');
-		$sandbox = new LuaSandbox();
-		$lua = <<<CODE
+    protected function setUp()
+    {
+        $this->obj = new LuaSandbox();
+    }
+
+    /**
+     * @expectedException WebPower\LuaSandbox\LuaErrorException
+     */
+    function testInvalidLuaThrowsException()
+    {
+        $lua = <<<CODE
 callUnExistingFunction();
 CODE;
-		$sandbox->execute($lua);
-	}
+        $this->obj->run($lua);
+    }
 
-	function testValidLua()
-	{
-		$sandbox = new LuaSandbox();
-		$res = $sandbox->execute(<<<CODE
+    function testValidLua()
+    {
+        $res = $this->obj->run(<<<CODE
 return 10 * 2;
 CODE
-);
-		$this->assertEquals(20, $res);
-	}
+        );
+        $this->assertEquals(20, $res);
+    }
 
-	function testCleanGlobalScope()
-	{
-		$sandbox = new LuaSandbox();
-		$globals = $sandbox->execute(<<<CODE
-local seen = {}
-function dumpGlobals(t, prefix)
-	seen[t] = true
-	local names = {}
-	for name in pairs(t) do
-		table.insert(names, prefix .. name)
-		v = t[name]
-		if type(v)=="table" and not seen[v] then
-			local sub = dumpGlobals(v, name .. '.')
-			for sub_k,sub_v in pairs(sub) do
-				names[sub_k] = sub_v
-			end
-		end
-	end
-	return names
+    function testUnset()
+    {
+        $this->obj->unsetVar(
+            array(
+                'dofile',
+                'loadfile',
+                'module',
+                'require',
+                'coroutine',
+                'debug',
+                'file',
+                'io',
+                'os',
+                'package',
+            )
+        );
+
+        $globals = $this->obj->run(<<<CODE
+local names = {}
+for name, val in pairs(_G) do
+    table.insert(names, name)
 end
-local globals = dumpGlobals(_G, '')
-table.sort(globals)
-return globals
+table.sort(names)
+return names
 CODE
-);
-		$this->assertEquals(
-			array(
-				1 => '_G',
-				'_VERSION',
-				'assert',
-				// 'collectgarbage',
-				// 'coroutine',
-				// 'coroutine.create',
-				// 'coroutine.running',
-				// 'debug',
-				// 'dofile',
-				'dumpGlobals', // Our function to dump the globals
-				'error',
-				'string',
-				'table'
-			),
-			$globals
-		);
-	}
+        );
+        $this->assertEquals(
+            array(
+                1 => '_G',
+                '_VERSION',
+                'assert',
+                'collectgarbage',
+                'error',
+                'gcinfo',
+                'getfenv',
+                'getmetatable',
+                'ipairs',
+                'load',
+                'loadstring',
+                'math',
+                'newproxy',
+                'next',
+                'pairs',
+                'pcall',
+                'print',
+                'rawequal',
+                'rawget',
+                'rawset',
+                'select',
+                'setfenv',
+                'setmetatable',
+                'string',
+                'table',
+                'tonumber',
+                'tostring',
+                'type',
+                'unpack',
+                'xpcall',
+            ),
+            $globals
+        );
+    }
 
-	function testAssertThrowsException()
-	{
-		$this->setExpectedException('\WebPower\LuaSandbox\LuaErrorException');
-		$sandbox = new LuaSandbox();
-		$sandbox->execute(<<<CODE
+    /**
+     * @expectedException WebPower\LuaSandbox\LuaErrorException
+     */
+    function testAssertThrowsException()
+    {
+        $this->setExpectedException('\WebPower\LuaSandbox\LuaErrorException');
+        $this->obj->run(<<<CODE
 assert(false, 'Assertion failed')
 CODE
-);
-	}
+        );
+    }
 
-	function testErrorThrowsException()
-	{
-		$this->setExpectedException('\WebPower\LuaSandbox\LuaErrorException');
-		$sandbox = new LuaSandbox();
-		$sandbox->execute(<<<CODE
+    /**
+     * @expectedException WebPower\LuaSandbox\LuaErrorException
+     */
+    function testErrorThrowsException()
+    {
+        $this->obj->run(<<<CODE
 error('Some error')
 CODE
-		);
-	}
+        );
+    }
+
+    function testPhpCallbackFunction()
+    {
+        $args = false;
+        $this->obj->assignCallable('doSomething', function() use(&$args) {
+                $args = func_get_args();
+            });
+        $this->obj->run(<<<CODE
+doSomething(1, 3, 3, 7, {1, 3, 3, 7})
+CODE
+        );
+        $this->assertEquals(
+            array(
+                1, 3, 3, 7,
+                array(1=> 1, 3, 3, 7)
+            ),
+            $args
+        );
+    }
+
+    /**
+     * @expectedException WebPower\LuaSandbox\Exception
+     */
+    function testInvalidCallbackName()
+    {
+        $this->obj->assignCallable('0abc', function() {});
+    }
+
+    function testVariables()
+    {
+        $this->obj->assignVar('valueFromPhp', 1337);
+        $val = $this->obj->run(<<<CODE
+return valueFromPhp
+CODE
+        );
+        $this->assertEquals(1337, $val);
+    }
+
+    /**
+     * @expectedException WebPower\LuaSandbox\InvalidVariableNameException
+     */
+    function testInvalidVariableName()
+    {
+        $this->setExpectedException('WebPower\LuaSandbox\Exception');
+        $this->obj->assignVar('0abc', 1337);
+    }
+
+    /**
+     * @expectedException WebPower\LuaSandbox\Exception
+     */
+    function testInvalidVariableValue()
+    {
+        $this->obj->assignVar('testFunc', function($a, $b) { return $a + $b; });
+    }
+
+    /**
+     * @expectedException WebPower\LuaSandbox\InvalidVariableNameException
+     */
+    function testReservedKeywordVariable()
+    {
+        $this->obj->assignVar('break', 'test');
+    }
 }
